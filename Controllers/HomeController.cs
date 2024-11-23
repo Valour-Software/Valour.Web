82c0eca2f499a9cd0e5762fca075027d8679e553
Kobe Albright <kobe.albright@gmail.com>
2024-11-23 00:30:28 -0500

    Web3 Page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -29,6 +29,12 @@ public class HomeController : Controller
     {
         return View();
     }
+    
+    [HttpGet("/web3")]
+    public IActionResult Web3()
+    {
+        return View();
+    }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
